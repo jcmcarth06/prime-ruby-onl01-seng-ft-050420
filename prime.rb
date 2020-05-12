d8dea120bbecab750c4172063b43105ebd605569
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-12 19:17:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,12 @@
 # Add  code here!
 def prime?(number)
-  number >= 2
-  number.each do |prime|
-    p prime
-  end
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |num_to_test|
+      number % num_to_test == 0
+    end
+  else
+    false
+  end  
 end
